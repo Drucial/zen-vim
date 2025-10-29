86bc206d47a7947c13aefefe7378cdf1834e8c93
Drucial <drucial@Mac.attlocal.net>
2025-10-29 18:05:43 -0400

    add web-dev-icons fallback

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,6 +1,7 @@
 return {
   { "nvim-lua/plenary.nvim", lazy = true },
   { "b0o/schemastore.nvim", lazy = true }, -- JSON schemas for better completion
+  { "nvim-tree/nvim-web-devicons", lazy = true }, -- Fallback icons for plugins that don't support mini.icons
 
   -- you can use the VeryLazy event for things that can
   -- load later and are not important for the initial UI
