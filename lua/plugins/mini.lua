430a3f8d4c4d6216374ca3bf10ee6f1d82b32530
Drucial <drucial@Mac.attlocal.net>
2025-11-17 14:37:43 -0500

    nvim-autopairs

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -2,9 +2,6 @@ return {
 	"nvim-mini/mini.nvim",
 	version = "*",
 	config = function()
-		-- mini.pairs - Auto-pair brackets, quotes, etc.
-		require("mini.pairs").setup()
-
 		-- mini.comment - Better commenting with JSX/TSX support
 		require("mini.comment").setup({
 			options = {
